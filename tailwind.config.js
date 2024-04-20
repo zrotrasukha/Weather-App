c40eecc9904a90c7563c28c21c9bc61e848d5ec3
Shivang Sagar <zrotrasukha@gmail.com>
2024-04-21 04:16:08 +0530

    Deleted all the CSS and used Tailwind for dummy
    I deleted all the css file and used tailwind instaed because I just
    wanted to work fast
    
    I did it because I wanted to work fast, instead here I am sitting on my
    chair for 2 hours and it is like 4am + though, I have understood how
    fast I can get with tailwind, it was fun, the next thing I got to do is
    add the damn js for the project, building with tutorials was hella easy
    but like this haha it is a different kind of pleasure man, I love it.
    Guess I was not supposed to add this message XD.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./distribution/*.html"],
+  theme: {
+    extend: {},
+    fontFamily: { 
+      'urbanist': ['Urbanist', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
